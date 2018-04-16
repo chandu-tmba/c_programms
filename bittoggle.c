f60b532953d911ed542d88439c90f2e870e2211a
Chandra Sekhar Temmanaboina <37570335+chandu-tmba@users.noreply.github.com>
2018-04-16 12:57:42 +0530

    Update bittoggle.c

--- bittoggle.c
+++ bittoggle.c
@@ -1,29 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
-
 int main()
 {
-
 	int i,x=10;
 	int bit_pos = 23;
 	unsigned int set_var = 0xFFFFFFFF;	
-
-
 	for(i=0;i<32;i++)
 		printf("%d",(1&(x>>i)));
-	printf("\n");
-	
-
+	printf("\n");	
  	int diff = 31-bit_pos;
-
 	set_var = set_var>>diff;
 	x=x^(set_var);
-
 	for(i=0;i<32;i++)
                 printf("%d",(1&(x>>i)));
-
 	printf("\n");
 
 }
